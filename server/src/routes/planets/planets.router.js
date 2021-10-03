c837f22d97ac9cdfaf5b46de492de0d719c38291
Rashid Armand <rashidharmand@gmail.com>
2021-10-03 13:29:24 -0400

    update: mounting resource routers to corresponding route to make route declaration in the controllers more concise

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -4,6 +4,6 @@ const planetsRouter = express.Router();
 
 const { httpGetAllPlanets } = require('./planets.controller');
 
-planetsRouter.route('/planets').get(httpGetAllPlanets);
+planetsRouter.route('/').get(httpGetAllPlanets);
 
 module.exports = planetsRouter;
