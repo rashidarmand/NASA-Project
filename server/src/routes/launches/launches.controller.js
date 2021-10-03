6677eabec2fb019e17c8d6103b4533f4bc00dc5c
Rashid Armand <rashidharmand@gmail.com>
2021-10-03 14:15:21 -0400

    update: added validation to add new launch route

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,32 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
+  const requiredLaunchDetails = [
+    'mission',
+    'rocket',
+    'launchDate',
+    'destination',
+  ];
+  const missingLaunchDetails = requiredLaunchDetails.filter((detail) => {
+    const launchInput = Object.entries(launch);
+    return (
+      !launchInput.some((val) => val[0] === detail) ||
+      launchInput.some((val) => val[0] === detail && val[1] === undefined)
+    );
+  });
+  if (missingLaunchDetails.length > 0) {
+    const message = missingLaunchDetails.map(
+      (launchDetail) => `${launchDetail} is required.`
+    );
+    return res.status(400).json({ error: 'Missing Launch Details.', message });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (launch.launchDate.toString() === 'Invalid Date') {
+    return res.status(400).json({ error: 'Invalid Launch Date' });
+  }
+
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
