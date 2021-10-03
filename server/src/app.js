c5804e401f9ad7eb07ddbb3efdbc11295b987caa
Rashid Armand <rashidharmand@gmail.com>
2021-10-03 11:56:06 -0400

    feature: morgan for logging server requests

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const { planetsRouter } = require('./routes/planets/planets.router');
 
@@ -12,13 +13,13 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
 app.get('/', (req, res) => {
-  // Automatically serve index.html on root path
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
