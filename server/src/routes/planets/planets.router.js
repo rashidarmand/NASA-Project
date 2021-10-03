fe1c6b5c9cb82887e5394a2d9f0807c54140b53e
Rashid Armand <rashidharmand@gmail.com>
2021-10-03 12:41:58 -0400

    fix: default export for planets router

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -6,6 +6,4 @@ const { getAllPlanets } = require('./planets.controller');
 
 planetsRouter.route('/planets').get(getAllPlanets);
 
-module.exports = {
-  planetsRouter,
-};
+module.exports = planetsRouter;
