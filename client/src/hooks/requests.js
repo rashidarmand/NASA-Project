bceb9f8eaf35a2fb064da2ee6fdb7d92e1d56356
Rashid Armand <rashidharmand@gmail.com>
2021-10-03 12:44:16 -0400

    feat: launches - milestone - frontend request for launches working

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,14 +1,17 @@
 const API_URL = 'http://localhost:8000';
 
+// Load planets and return as JSON
 async function httpGetPlanets() {
   const req = await fetch(`${API_URL}/planets`);
-  const res = await req.json();
-  return res;
+  const planets = await req.json();
+  return planets;
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const req = await fetch(`${API_URL}/launches`);
+  const launches = await req.json();
+  return launches.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
 async function httpSubmitLaunch(launch) {
