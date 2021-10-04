924c99b4e1f85d613f77805ceecac8f75195ab4f
Rashid Armand <rashidharmand@gmail.com>
2021-10-03 23:23:35 -0400

    feat: mongodb - created launches schema

---
+++ server/src/models/launches.mongoose.js
@@ -0,0 +1,41 @@
+const { Schema } = require('mongoose');
+
+const LaunchesSchema = new Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: {
+    type: [String],
+    default: ['Elon Musk', 'SpaceX'],
+    required: true,
+  },
+  upcoming: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+});
+
+module.exports = LaunchesSchema;
