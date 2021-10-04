4addce6d16d5d341b92c50516be98c32dad1a074
Rashid Armand <rashidharmand@gmail.com>
2021-10-04 02:55:20 -0400

    feat: testing - abstracted mongo connection to separate file to use connection in tests

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,16 @@
 require('dotenv').config();
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.MONGO_URL;
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
